c9dacdea950a8b8aa72aa81e8ed1e5e84be17f59
Misha0966 <108358743+Misha0966@users.noreply.github.com>
2022-08-20 13:12:13 +0300

    Задача 62*
    Задача 62. Заполните спирально массив 4 на 4.
    
    Например, на выходе получается вот такой массив:
    
    1 2 3 4
    
    12 13 14 5
    
    11 16 15 6
    
    10 9 8 7

---
+++ Масссив 4 на 4, заполненный спирально.cs
@@ -0,0 +1,45 @@
+﻿// 1 2 3 4
+// 12 13 14 5
+// 11 16 15 6
+// 10 9 8 7
+
+// ввод входных условий задачи
+int n = 4;
+int temp = 1;
+int i = 0;
+int j = 0;
+
+int[,] arraySpiral = new int[n, n]; // создание спирального массива
+while (temp <= arraySpiral.GetLength(0) * arraySpiral.GetLength(1))
+{
+  arraySpiral[i, j] = temp;
+  temp++;
+  if (i <= j + 1 
+        && i + j < arraySpiral.GetLength(1) - 1)
+        j++;
+
+  else if (i < j    
+        && i + j >= arraySpiral.GetLength(0) - 1)
+        i++;
+  else if (i >= j 
+        && i + j > arraySpiral.GetLength(1) - 1)
+        j--;
+  else
+        i--;
+}
+PrintArray(arraySpiral);
+
+void PrintArray (int[,] array) // метод печати массива
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      if (array[i,j] / 10 <= 0)
+      Console.Write($" {array[i,j]} ");
+
+      else Console.Write($"{array[i,j]} ");
+    }
+    Console.WriteLine();
+  }
+}
